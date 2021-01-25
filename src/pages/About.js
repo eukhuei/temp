7574fda6aca72a51f6e0b21e25c88f77bb11e3c1
ehsia <eukhuei@live.com.my>
2021-01-25 10:50:25 +0800

    Add in the code

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>Hello World. Good bye</p>
+    </section>
+  )
+}
+
+export default About
